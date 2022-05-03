2b60ce6c2dab8d08e8f94d25fd166b48f410fc4b
tokitsu <48195108+tokitsu@users.noreply.github.com>
2022-05-03 14:29:57 +0900

    add bank function with usereducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./styles.css";
+import React from "react";
+import Bank from "./pages/bank/Bank";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>useReducer</h2>
+      <Bank />
     </div>
   );
 }
